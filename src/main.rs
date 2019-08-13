401747f77c8406d5eb7ae4b8c0ddbba48c313c79
lcnr/Bastian Kauschke <bastian_kauschke@hotmail.de>
2019-08-13 22:36:56 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+mod aks;
 mod is_prime;
 mod miller_rabin;
-mod aks;
 mod poly;
 
 use num_bigint::BigUint;
@@ -8,20 +8,24 @@ use num_bigint::BigUint;
 use std::io::{stdout, Write};
 
 fn radix(x: &BigUint, radix: u32) -> String {
-    x.to_radix_be(radix).into_iter().map(|c| std::char::from_digit(c as u32, radix).unwrap()).collect()
+    x.to_radix_be(radix)
+        .into_iter()
+        .map(|c| std::char::from_digit(c as u32, radix).unwrap())
+        .collect()
 }
 
 fn truncatable(base: u32, mut primality_check: impl FnMut(&BigUint) -> bool) {
     print!("Base {}: ", base);
-    
-    let mut numbers: Vec<_> = (2..base).filter_map(|n| {
-        if (2..n).any(|i| n % i == 0) {
-            None
-        }
-        else {
-            Some(BigUint::from(n))
-        }
-    }).collect();
+
+    let mut numbers: Vec<_> = (2..base)
+        .filter_map(|n| {
+            if (2..n).any(|i| n % i == 0) {
+                None
+            } else {
+                Some(BigUint::from(n))
+            }
+        })
+        .collect();
 
     let mut biggest_prime = numbers.last().unwrap().clone();
 
@@ -33,17 +37,21 @@ fn truncatable(base: u32, mut primality_check: impl FnMut(&BigUint) -> bool) {
         total += numbers.len();
         stdout().flush().unwrap();
 
-        numbers = numbers.iter().flat_map(|b| {
-            (1..base).flat_map(|n| {
-                let num = b + (&offset * n);
-                if primality_check(&num) { 
-                    Some(num) 
-                }
-                else { 
-                    None 
-                }
-            }).collect::<Vec<_>>()
-        }).collect();
+        numbers = numbers
+            .iter()
+            .flat_map(|b| {
+                (1..base)
+                    .flat_map(|n| {
+                        let num = b + (&offset * n);
+                        if primality_check(&num) {
+                            Some(num)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect();
 
         biggest_prime = numbers.last().map_or(biggest_prime, |num| num.clone());
 
@@ -53,18 +61,21 @@ fn truncatable(base: u32, mut primality_check: impl FnMut(&BigUint) -> bool) {
             print!(", ");
         }
     }
-    println!("\nThe biggest truncatable prime in base {} with {} digits is {}", base, digits, radix(&biggest_prime, base));
-    println!("There are a total of {} truncatable primes in base {}\n", total, base);
+    println!(
+        "\nThe biggest truncatable prime in base {} with {} digits is {}",
+        base,
+        digits,
+        radix(&biggest_prime, base)
+    );
+    println!(
+        "There are a total of {} truncatable primes in base {}\n",
+        total, base
+    );
 }
-
 
 fn main() {
     let aks = false;
-    let check = if aks {
-        aks::aks
-    } else {
-        is_prime::is_prime
-    };
+    let check = if aks { aks::aks } else { is_prime::is_prime };
 
     for base in 3.. {
         truncatable(base, check);
